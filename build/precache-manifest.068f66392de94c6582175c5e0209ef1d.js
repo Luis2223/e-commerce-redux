502bb0f221dea9b0fe8d9b3d66d6e2cce7042d35
Luis2223 <luis.santos@grupozelo.com>
2020-10-01 12:05:04 -0300

    build

---
+++ build/precache-manifest.068f66392de94c6582175c5e0209ef1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b46be5fb38ce47c2e9884e4b9550a86f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c637d9900979d0310617",
+    "url": "/static/js/2.6b18da43.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6b18da43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eecb2d733caaf1b2fb2",
+    "url": "/static/js/main.69896f15.chunk.js"
+  },
+  {
+    "revision": "e54d79d945a7d2daa143",
+    "url": "/static/js/runtime-main.569c20a7.js"
+  },
+  {
+    "revision": "3c9a1ce505a48f2c15ba9505b6f5667b",
+    "url": "/static/media/background.3c9a1ce5.svg"
+  },
+  {
+    "revision": "879557c5bdbbd3743a7680c651e31d08",
+    "url": "/static/media/logo.879557c5.svg"
+  }
+]);
